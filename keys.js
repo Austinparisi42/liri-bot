8b1f2a2d8c02f256092a0700297860067ede60c1
Austin Parisi <austinparisi1991@gmail.com>
2017-10-08 15:02:15 -0500

    done

---
+++ keys.js
@@ -0,0 +1,18 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'yFScwbTeFXv7BvxMS4QR3GkQV',
+  consumer_secret: 'Ai8zR9vk7n18E44IRPCqayMMZZgcIyJfHE4bKCnlAlP9ol1i9N',
+  access_token_key: '916034756290195461-BERuyYYg3sfhoqXYdk2bn5I39ROtgjI',
+  access_token_secret: 'iIVKEy78aVVQA5ovi9S1jvxhtlQWxGa0xXUkxMar8yStG',
+};
+
+
+
+var spotifyKeys = {
+	id: 'd4f6935cb86044678022afdbbab82192',
+	secret: 'adc26855f67f4424a35f3999a3312a6c'
+};
+
+module.exports = {twitterKeys, spotifyKeys};
+
---
+++ liri.js
@@ -0,0 +1,96 @@
+var fs = require("fs");
+var request = require("request");
+var twitter = require("twitter");
+var spotify = require("node-spotify-api");
+var keys = require("./keys.js");
+var firstArg = process.argv[2];
+
+if (firstArg === "my-tweets") {
+    runTwitter();
+} else if (firstArg === "spotify-this-song") {
+    if (process.argv[3]) {
+        var value = "";
+        for (var n = 3; n < process.argv.length; n++) {
+            value += " " + process.argv[n];
+        }
+    };
+    runSpotify();
+} else if (firstArg === "movie-this") {
+    if (process.argv[3]) {
+        var value = "";
+        for (var n = 3; n < process.argv.length; n++) {
+            value += " " + process.argv[n];
+        }
+    };
+    runOMDB();
+} else if (firstArg === "do-what-it-says") {
+    runDoIt();
+} else {
+    console.log("Liri is not sure where you are asking for. Try again!");
+}
+
+function runTwitter() {
+    var client = new twitter(keys.twitterKeys);
+    var params = { screen_name: 'gout_squad' };
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (!error) {
+            for (var i = 0; i < tweets.length; i++) {
+                var date = tweets[i].created_at;
+
+                console.log("@gout_squad: " + tweets[i].text + " Created at: " + date);
+            }
+        } else {
+            console.log(error);
+        }
+    });
+}
+
+function runSpotify() {
+    var spotifyAPI = new spotify(keys.spotifyKeys);
+    spotifyAPI.search({ type: 'track', query: value || "The Sign Ace of Base" }, function(err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        } else {
+            console.log("Artist: " + data.tracks.items[0].artists[0].name);
+            console.log("Song: " + data.tracks.items[0].name);
+            console.log("Album: " + data.tracks.items[0].album.name);
+            console.log("Preview Here: " + data.tracks.items[0].preview_url);
+        }
+    });
+}
+
+function runOMDB() {
+    if (value === undefined) {
+        value = "Mr.Nobody";
+    }
+    var request = require("request");
+    request("http://www.omdbapi.com/?t=" + value + "&apikey=40e9cece", function(error, response, body) {
+        if (!error && response.statusCode === 200) {
+            console.log("Title: " + JSON.parse(body).Title);
+            console.log("Year: " + JSON.parse(body).Year);
+            console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+            console.log("RT Rating: " + JSON.parse(body).Ratings[1].Value);
+            console.log("Country: " + JSON.parse(body).Country);
+            console.log("Language: " + JSON.parse(body).Language);
+            console.log("Plot: " + JSON.parse(body).Plot);
+            console.log("Actors: " + JSON.parse(body).Actors);
+        }
+    });
+}
+
+function runDoIt() {
+    fs.readFile("random.txt", "utf8", function(error, data) {
+        if (error) {
+            return console.log(error);
+        } else {
+            var spotifyAPI = new spotify(keys.spotifyKeys);
+            var dataArr = data.split(",");
+            spotifyAPI.search({ type: 'track', query: dataArr[1] }, function(err, data) {
+                console.log("Artist: " + data.tracks.items[0].artists[0].name);
+                console.log("Song: " + data.tracks.items[0].name);
+                console.log("Album: " + data.tracks.items[0].album.name);
+                console.log("Preview Here: " + data.tracks.items[0].preview_url)
+            });
+        }
+    });
+}
